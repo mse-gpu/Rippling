b820df810bb9b82cb594adc2d3e8cb167dabcaba
Baptiste Wicht <baptiste.wicht@gmail.com>
2012-03-01 21:11:40 +0100

    Set the prototype for the Ripling application

--- src/cpp/core/omp/ripling/Ripling.cpp
+++ src/cpp/core/omp/ripling/Ripling.cpp
@@ -1,8 +1,31 @@
 #include <iostream>
 #include "omp.h"
 
+#include "Domaines.h"
+
 extern int launchApplication(){
     std::cout << "Launch the application" << std::endl;
+
+    char* argv = NULL;
+    GLUTWindowManagers::init(0, argv);
+
+    float xMin = -5;
+    float xMax = +5;
+    float yMin = -5;
+    float yMax = +5;
+
+    Domaines domain(xMin, yMin, xMax - xMin, yMax - yMin);
+   
+    int w = 800;
+    int h = 800;
+
+    RiplingFunctionalImage* functionalImage = new RiplingFunctionalImage(w,h,domain);
+    RiplingFunctionSelections* functionSelections = new RiplingFunctionSelections(functionImage);
+
+    GLUTWindowManagers* windowManager = GLUTWindowManagers::getInstance();
+    windowManager->createWindow(functionalImage);
+    windowManager->runAll();
+
 
     return 0;
 }
