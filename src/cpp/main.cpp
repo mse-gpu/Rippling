87e193efd4c158cf14d1517cbc77ab97352d33c8
Baptiste Wicht <baptiste.wicht@gmail.com>
2012-04-05 18:55:04 +0200

    Implement a benchmark for both versions

--- src/cpp/main.cpp
+++ src/cpp/main.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <limits.h>
+#include <math.h>
 
 #include "omp.h"
 
@@ -57,20 +58,87 @@ void display(RipplingImage* image){
     delete glImage;
 }
 
+#define DIM_BENCH 50000
+#define THREADS 24
+
+float d(float x, float y){
+    float fx = x - (DIM_BENCH / 2);
+    float fy = y - (DIM_BENCH / 2);
+
+    return sqrt(fx * fx + fy * fy);
+}
+
+float color(float x, float y, float t){
+    return 128 + 127 * ((cos(d(x,y) / (float)10 -(t / (float)7))) / (d(x,y) / 10 + 1));
+}
+
+inline void benchSequential(){
+    DomaineMaths domain(0, 0, DIM_BENCH, DIM_BENCH);
+
+    float dx = (float) (domain.dx / (float) DIM_BENCH);
+    float dy = (float) (domain.dy / (float) DIM_BENCH);
+    float y = domain.y0;
+
+    float t = 1;
+
+    for(int i = 1; i <= DIM_BENCH; ++i){
+	float x = domain.x0;
+
+	for(int j = 1; j <= DIM_BENCH; ++j){
+	    float c = color(x,y,t);
+
+	    x += dx;
+	}
+
+	y += dy;
+    }
+}
+
+inline void benchParallel(){
+    DomaineMaths domain(0, 0, DIM_BENCH, DIM_BENCH);
+
+    const float dx = (float) (domain.dx / (float) DIM_BENCH);
+    const float dy = (float) (domain.dy / (float) DIM_BENCH);
+
+    float t = 1;
+
+    #pragma omp parallel
+    {
+	int tid = omp_get_thread_num();
+	int i = tid + 1;
+
+	float y = domain.y0 + tid * dy;
+
+	while(i <= DIM_BENCH){
+	    float x = domain.x0;
+
+	    for(int j = 1; j <= DIM_BENCH; ++j){
+		float c = color(x,y,t);
+
+		x += dx;
+	    }
+
+	    y += THREADS * dy;
+
+	    i += THREADS;
+	}
+    }
+}
+
 int bench(){
     std::cout << "Launch a benchmark" << std::endl;
 
     Chronos chronos;
     chronos.start();
 
-
+    benchSequential();
 
     chronos.stop();
     chronos.print("Sequential Version");
 
     chronos.start();
 
-
+    benchParallel();
 
     chronos.stop();
     chronos.print("OMP Version");
